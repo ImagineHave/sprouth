62be0235ea97b88523625610d1eadf7061b0b285
Chris Williams <christopher.williams@imagine-have.xyz>
2018-12-09 15:26:36 +0000

    start

---
+++ src/main/java/xyz/imaginehave/sprouth/sprouth/SprouthApplication.java
@@ -0,0 +1,12 @@
+package xyz.imaginehave.sprouth.sprouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprouthApplication.class, args);
+	}
+}
